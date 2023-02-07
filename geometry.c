744cd07c8bc503265fbe9fd8290d8b8e1afd9fc3
Nikita Biryukov <chppppr@yandex.ru>
2023-02-07 18:59:38 +0700

    Implement read numbers from stdin

--- geometry.c
+++ geometry.c
@@ -24,19 +24,63 @@ void to_lower_string(char* string)
     }
 }
 
+double get_number(int* column)
+{
+    char temp[25];
+    char ch;
+    int point_count = 0, i = 0;
+
+    while ((ch = getchar()) != ' ') {
+        temp[i] = ch;
+
+        if (temp[i] == '.') {
+            point_count++;
+            if (point_count > 1) {
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        if (temp[i] == ')') {
+            i++;
+            break;
+        }
+
+        if (temp[i] == ',') {
+            i++;
+            break;
+        }
+
+        i++;
+    }
+    *column += i + 1;
+    char* eptr;
+    return strtod(temp, &eptr);
+}
+
+void take_info_circle(Circle* circle, int* column)
+{
+    circle->point.x = get_number(column);
+    circle->point.y = get_number(column);
+    getchar();
+    circle->raduis = get_number(column);
+}
+
 int main()
 {
     char geom[NAME_SIZE];
     char ch;
-    int column = 0;
+    int column;
 
     puts("Enter a geometric shape (or q for exit):");
     while ((ch = getchar()) != '\n' && ch != 'q') {
+        column = 0;
         do {
-            if (ch == '(' || ch == ' ') {
+            if (ch == '(') {
                 if (strcmp(geom, "circle") == 0) {
+                    Circle circle;
                     to_lower_string(geom);
-                    puts(geom);
+                    take_info_circle(&circle, &column);
+
                     break;
                 } else {
                     puts("Error: expected 'circle'");
