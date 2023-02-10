9732089877ca30ecf410c154a8a7237d65ff8748
Nikita Biryukov <chppppr@yandex.ru>
2023-02-10 23:27:27 +0700

    Implement input from file

--- geometry.c
+++ geometry.c
@@ -23,11 +23,20 @@ enum Errors {
     ER_BACK_BRACE,
     ER_UNEXPECT_TOKEN,
     ER_EXPECT_COMMA,
-    ER_UNEXPECT_COMMA
+    ER_UNEXPECT_COMMA,
+    _FILE,
+    NOT_FILE
 };
 
-void print_error(int column, int status)
+void print_error(int column, int status, int is_file, FILE* file)
 {
+    if (is_file == _FILE) {
+        char temp[256];
+        fseek(file, -column - 1, SEEK_CUR);
+        fgets(temp, 255, file);
+        printf("\n%s", temp);
+    }
+
     for (int i = 0; i < column; i++) {
         putchar(' ');
     }
@@ -61,119 +70,155 @@ void to_lower_string(char* string)
     }
 }
 
-void del_space(int* column)
+void del_space(int* column, FILE* file)
 {
     char ch;
-    while ((ch = getchar()) == ' ') {
+    while ((ch = getc(file)) == ' ') {
         *column += 1;
         continue;
     }
     if (ch != ' ')
-        ungetc(ch, stdin);
+        ungetc(ch, file);
 }
 
-double get_number(int* column)
+double get_number(int* column, int is_file, FILE* file)
 {
     char temp[25];
     char ch;
     int point_count = 0, i = 0;
 
-    del_space(column);
+    del_space(column, file);
 
-    while ((ch = getchar()) != ' ') {
+    while ((ch = getc(file)) != ' ') {
         temp[i] = ch;
 
         if (temp[i] == '.') {
             point_count++;
             if (point_count > 1) {
-                print_error(*column + i + 1, ER_NOT_DOUBLE);
+                if (is_file == _FILE)
+                    print_error(*column + i + 1, ER_NOT_DOUBLE, _FILE, file);
+                else
+                    print_error(*column + i + 1, ER_NOT_DOUBLE, NOT_FILE, file);
                 exit(EXIT_FAILURE);
             }
         }
 
         if (temp[i] == ')') {
-            ungetc(temp[i], stdin);
+            ungetc(temp[i], file);
             i++;
             break;
         }
 
         if (temp[i] == ',') {
-            ungetc(temp[i], stdin);
+            ungetc(temp[i], file);
             break;
         }
 
         if (temp[i] == '(') {
             i++;
-            print_error(*column + i, ER_BACK_BRACE);
+            if (is_file == _FILE)
+                print_error(*column + i, ER_BACK_BRACE, _FILE, file);
+            else
+                print_error(*column + i, ER_BACK_BRACE, NOT_FILE, file);
             exit(EXIT_FAILURE);
         }
 
         if (!isdigit(temp[i]) && temp[i] != '.') {
             i++;
-            print_error(*column + i, ER_NOT_DOUBLE);
+            if (is_file == _FILE)
+                print_error(*column + i, ER_NOT_DOUBLE, _FILE, file);
+            else
+                print_error(*column + i, ER_NOT_DOUBLE, NOT_FILE, file);
             exit(EXIT_FAILURE);
         }
 
         i++;
     }
-    del_space(column);
+    del_space(column, file);
     *column += i + 1;
     char* eptr;
     return strtod(temp, &eptr);
 }
 
-bool expect(char expect, int* column, int status)
+bool expect(char expect, int* column, int status, int is_file, FILE* file)
 {
     char ch;
-    if ((ch = getchar()) == expect) {
+    if ((ch = getc(file)) == expect) {
         return true;
     } else {
-        print_error(*column, status);
+        if (is_file == _FILE)
+            print_error(*column, status, _FILE, file);
+        else
+            print_error(*column, status, NOT_FILE, file);
         exit(EXIT_FAILURE);
     }
 }
 
-bool unexpect(char unexpect, int* column, int status)
+bool unexpect(char unexpect, int* column, int status, int is_file, FILE* file)
 {
     char ch;
-    if ((ch = getchar()) == unexpect) {
-        print_error(*column, status);
+    if ((ch = getc(file)) == unexpect) {
+        if (is_file == _FILE)
+            print_error(*column, status, _FILE, file);
+        else
+            print_error(*column, status, NOT_FILE, file);
         exit(EXIT_FAILURE);
     }
-    ungetc(ch, stdin);
+    ungetc(ch, file);
     return true;
 }
 
-void get_point(Point* point, int* column)
+void get_point(Point* point, int* column, int is_file, FILE* file)
 {
-    point->x = get_number(column);
-    unexpect(',', column, ER_UNEXPECT_COMMA);
+    if (is_file == _FILE) {
+        point->x = get_number(column, _FILE, file);
+        unexpect(',', column, ER_UNEXPECT_COMMA, _FILE, file);
 
-    point->y = get_number(column);
-    expect(',', column, ER_EXPECT_COMMA);
+        point->y = get_number(column, _FILE, file);
+        expect(',', column, ER_EXPECT_COMMA, _FILE, file);
+    } else {
+        point->x = get_number(column, NOT_FILE, file);
+        unexpect(',', column, ER_UNEXPECT_COMMA, NOT_FILE, file);
+
+        point->y = get_number(column, NOT_FILE, file);
+        expect(',', column, ER_EXPECT_COMMA, NOT_FILE, file);
+    }
 }
 
-void end_of_line(int* column)
+void end_of_line(int* column, int is_file, FILE* file)
 {
     char ch;
-    while ((ch = getchar()) != '\n' && ch != EOF) {
+    while ((ch = getc(file)) != '\n' && ch != EOF) {
         if (ch != ' ') {
-            print_error(*column, ER_UNEXPECT_TOKEN);
+            if (is_file == _FILE)
+                print_error(*column, ER_UNEXPECT_TOKEN, _FILE, file);
+            else
+                print_error(*column, ER_UNEXPECT_TOKEN, NOT_FILE, file);
             exit(EXIT_FAILURE);
         }
         *column += 1;
     }
 }
 
-void take_info_circle(Circle* circle, int* column)
+void take_info_circle(Circle* circle, int* column, int is_file, FILE* file)
 {
-    get_point(&circle->point, column);
+    if (is_file == _FILE) {
+        get_point(&circle->point, column, _FILE, file);
 
-    circle->raduis = get_number(column);
+        circle->raduis = get_number(column, _FILE, file);
 
-    expect(')', column, ER_BACK_BRACE);
+        expect(')', column, ER_BACK_BRACE, _FILE, file);
 
-    end_of_line(column);
+        end_of_line(column, _FILE, file);
+    } else {
+        get_point(&circle->point, column, NOT_FILE, file);
+
+        circle->raduis = get_number(column, NOT_FILE, file);
+
+        expect(')', column, ER_BACK_BRACE, NOT_FILE, file);
+
+        end_of_line(column, NOT_FILE, file);
+    }
 }
 
 void show_info_circle(Circle* circle)
@@ -184,38 +229,91 @@ void show_info_circle(Circle* circle)
            circle->raduis);
 }
 
-int main()
+void parser_stdin(FILE* stdin)
 {
     char geom[NAME_SIZE];
     char ch;
     int column;
 
     puts("Enter a geometric shape (or q for exit):");
-    while ((ch = getchar()) != EOF && ch != 'q') {
+    while ((ch = getc(stdin)) != EOF && ch != 'q') {
         column = 0;
         do {
             if (ch == '(' || ch == ' ') {
                 to_lower_string(geom);
                 if (strcmp(geom, "circle") == 0) {
                     Circle circle;
-                    take_info_circle(&circle, &column);
+                    take_info_circle(&circle, &column, NOT_FILE, stdin);
+                    printf("\nYou have entered: \n");
                     show_info_circle(&circle);
                     break;
                 } else {
-                    print_error(0, ER_NAME);
+                    print_error(0, ER_NAME, NOT_FILE, stdin);
                     exit(EXIT_FAILURE);
                 }
             }
 
             if (ch == ')') {
-                print_error(column, ER_BACK_BRACE);
+                print_error(column, ER_BACK_BRACE, NOT_FILE, stdin);
                 exit(EXIT_FAILURE);
             }
 
             geom[column++] = ch;
 
-        } while ((ch = getchar()) != '\n');
+        } while ((ch = getc(stdin)) != '\n');
         puts("Enter a new geometric shape (or q for exit):");
+    }
+}
+
+void parsFILE(FILE* file)
+{
+    char geom[NAME_SIZE];
+    char ch;
+    int column;
+
+    while ((ch = getc(file)) != EOF && ch != 'q') {
+        column = 0;
+        do {
+            if (ch == '(' || ch == ' ') {
+                to_lower_string(geom);
+                if (strcmp(geom, "circle") == 0) {
+                    Circle circle;
+                    take_info_circle(&circle, &column, _FILE, file);
+                    printf("\nYou have entered: \n");
+                    show_info_circle(&circle);
+                    break;
+                } else {
+                    print_error(0, ER_NAME, _FILE, file);
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            if (ch == ')') {
+                print_error(column, ER_BACK_BRACE, _FILE, file);
+                exit(EXIT_FAILURE);
+            }
+
+            geom[column++] = ch;
+
+        } while ((ch = getc(file)) != '\n');
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    FILE* file = NULL;
+    if (argc < 2)
+        parser_stdin(stdin);
+    else if (argc == 2) {
+        if ((file = fopen(argv[1], "r")) == NULL) {
+            printf("Error: can't open file \"%s\"\n", argv[1]);
+            exit(EXIT_FAILURE);
+        } else {
+            parsFILE(file);
+            fclose(file);
+        }
+    } else {
+        printf("Usage: %s <filename>\n", argv[0]);
     }
 
     return 0;
